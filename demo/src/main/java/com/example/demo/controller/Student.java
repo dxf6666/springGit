39509b5f49a72f875cfcab9eec4dac5b9868457c
tom <1682999@qq.com>
2018-12-24 16:11:16 +0800

    项目提交rrr

--- demo/src/main/java/com/example/demo/controller/Student.java
+++ demo/src/main/java/com/example/demo/controller/Student.java
@@ -2,6 +2,6 @@ package com.example.demo.controller;
 
 public class Student {
     public void str(){
-        System.out.println("嗷嗷嗷啊嗷嗷嗷啊嗄");
+        System.out.println("嗷嗷嗷啊嗷嗷嗷啊嗄------------");
     }
 }
